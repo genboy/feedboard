ddf05babc8bfd7293b1b6be368b4d8154eafdfd9
genboy <msg@genboy.net>
2022-08-03 00:09:49 +0200

    Separate opml sources to test combine  categories and fix empty (null) description variables

--- assets/xmlparser.php
+++ assets/xmlparser.php
@@ -1,7 +1,20 @@
 <?php
-header('Content-type: text/xml; charset=utf-8');
-
 $url = $_GET['url'];
-echo file_get_contents($url);
+
+/*
+function checkXMLContent( $url )
+{
+    $content = file_get_contents($url);
+    try { $rss = new SimpleXmlElement($content); }
+    catch(Exception $e){
+      return false;
+    }
+    return true;
+}
+*/
+//if( checkXMLContent( $url ) ){
+  header('Content-type: text/xml; charset=utf-8');
+  echo file_get_contents($url);
+//}
 
 ?>
